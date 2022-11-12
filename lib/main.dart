c8ebdddd8b0ab7d83361f35dd2ef6b42eb612094
sachinv26 <vermas7008@gmail.com>
2022-11-12 23:02:46 +0530

    onboarding screen and other changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:startupadda/main.dart';
 import 'firebase/firebase_options.dart';
-import './screens/Myhomepage.dart';
 
-void main()=>runApp(MyApp());
+void main() => runApp(MyApp());
+
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
@@ -21,5 +23,178 @@ class MyApp extends StatelessWidget {
   }
 }
 
+class MyhomePage extends StatefulWidget {
+  const MyhomePage({Key? key}) : super(key: key);
 
+  @override
+  State<MyhomePage> createState() => _MyhomePageState();
+}
 
+class _MyhomePageState extends State<MyhomePage> {
+  late PageController _pageController;
+
+  int _pageIndex = 0;
+  @override
+  void initState() {
+    // TODO: implement initState
+    _pageController = PageController(initialPage: 0);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    _pageController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              Expanded(
+                child: PageView.builder(
+                  itemCount: demo_data.length,
+                  controller: _pageController,
+                  onPageChanged: (index) {
+                    setState(() {
+                      _pageIndex=index;
+                    });
+                  },
+                  itemBuilder: (context, index) => OnboardContent(
+                    image: demo_data[index].image,
+                    title: demo_data[index].title,
+                    description: demo_data[index].description,
+                  ),
+                ),
+              ),
+              Row(
+                children: [
+                  ...List.generate(
+                    demo_data.length,
+                    (index) =>Padding(
+                      padding: const EdgeInsets.only(right: 4),
+                      child: DotIndicator(
+                        isActive: index==_pageIndex,
+                      ),
+                    ),
+                  ),
+                  const Spacer(),
+                  SizedBox(
+                    height: 60,
+                    width: 60,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        _pageController.nextPage(
+                            duration: const Duration(milliseconds: 300),
+                            curve: Curves.ease);
+                      },
+                      style: ElevatedButton.styleFrom(
+                        shape: const CircleBorder(),
+                      ),
+                      child: Image.asset(
+                        "assets/images/rightarrow.png",
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 20,
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class DotIndicator extends StatelessWidget {
+  const DotIndicator({
+    Key? key,
+    this.isActive = false,
+  }) : super(key: key);
+
+  final bool isActive;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: isActive ? 12 : 4,
+      width: 4,
+      decoration: BoxDecoration(
+        color: isActive? Colors.red : Colors.red.withOpacity(0.4),
+        borderRadius: BorderRadius.all(Radius.circular(12)),
+      ),
+    );
+  }
+}
+
+class Onboard {
+  final String image, title, description;
+
+  Onboard(
+      {required this.image, required this.title, required this.description});
+}
+
+final List<Onboard> demo_data = [
+  Onboard(
+      image: "assets/images/Startup1.png",
+      title: "first title",
+      description: "first description"),
+  Onboard(
+      image: "assets/images/Startup1.png",
+      title: "second title",
+      description: "second description"),
+  Onboard(
+      image: "assets/images/Startup1.png",
+      title: "third title",
+      description: "third description"),
+];
+
+class OnboardContent extends StatelessWidget {
+  const OnboardContent({
+    Key? key,
+    required this.image,
+    required this.title,
+    required this.description,
+  }) : super(key: key);
+
+  final String image, title, description;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        const Spacer(),
+        Image.asset(
+          image,
+          height: 250,
+        ),
+        const Spacer(),
+        Text(
+          title,
+          textAlign: TextAlign.center,
+          style: Theme.of(context)
+              .textTheme
+              .headline5!
+              .copyWith(fontWeight: FontWeight.w500),
+        ),
+        const SizedBox(
+          height: 16,
+        ),
+        Text(
+          description,
+          textAlign: TextAlign.center,
+        ),
+        const Spacer(),
+      ],
+    );
+  }
+}
